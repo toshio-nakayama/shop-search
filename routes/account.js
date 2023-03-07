7a508812a45edf3ca4f4557bf0c3e067a9f165e7
Toshio <toshio.h1990@gmail.com>
2023-03-07 10:21:14 +0900

    feat: authorization

--- routes/account.js
+++ routes/account.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { authenticate, authorize, PRIVILEGE } = require('../lib/security/accesscontrol.js');
 
-router.get('/', (req, res, next)=>{
+router.get('/', authorize(PRIVILEGE.NORMAL), (req, res, next) => {
   res.render('./account/index.ejs');
 });
 
@@ -10,5 +10,10 @@ router.get('/login', (req, res) => {
 });
 
 router.post('/login', authenticate());
+
+router.post('/logout', (req, res, next)=>{
+  req.logout();
+  res.redirect('/account/login');
+});
 
 module.exports = router;
